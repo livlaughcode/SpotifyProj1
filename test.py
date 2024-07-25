43215678ae814ac6f3405076491fe7434f7f65ab
Euan <75675713+yoo1n@users.noreply.github.com>
2024-07-25 21:33:03 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 textInput = Input("Would you like to cry?")
-if textInput == "yes" OR textInput == "Yes":
+if textInput == "yes" or textInput == "Yes":
   print("me too")
 else:
   print("study python harder")
